16f29e3e2afc938eb38338b755ffaa8f635af4e0
apiyo <franklineapiyo@gmail.com>
2023-10-03 20:58:02 +0300

    Make sure borders are correctly styled

--- src/main/advertisers/views.cljs
+++ src/main/advertisers/views.cljs
@@ -23,26 +23,27 @@
         error-state?
         @(re-frame/subscribe
           [::subs/error-state?])]
-    [:table {:class "bg-[#222] border-collapse border border-slate-500 w-11/12 m-5 mt-0.2 text-justify border-collapse"}
-     [:thead {:class "cursor-pointer"}
+    [:table {:class "bg-[#222] border-collapse border border-[#343434] w-11/12 m-5 mt-0.2 text-justify border-collapse cursor-pointer"}
+     [:thead
       [:tr
-       [:th {:class "border border-[#343434]-600 text-justify"
+       {:class "hover:bg-[#313131]"}
+       [:th {:class "border border-[#343434] text-justify border-x-0"
              :on-click #(re-frame/dispatch
                          [::events/sort-advertisers :name])}
         "ADVERTISER" (sorting-with-column :name)]
-       [:th {:class "border border-[#343434]-600 text-justify"
+       [:th {:class "border border-[#343434] text-justify border-x-0"
              :on-click #(re-frame/dispatch
                          [::events/sort-advertisers :createdAt])}
         "CREATION DATA" (sorting-with-column :createdAt)]
-       [:th {:class "border border-[#343434]-600 text-justify"
+       [:th {:class "border border-[#343434] text-justify border-x-0"
              :on-click #(re-frame/dispatch
                          [::events/sort-advertisers :campaign-id-count])}
         "# CAMPAIGNS" (sorting-with-column :campaign-id-count)]
-       [:th {:class "border border-[#343434]-600 text-justify"
+       [:th {:class "border border-[#343434] text-justify border-x-0"
              :on-click #(re-frame/dispatch
                          [::events/sort-advertisers :impressions])}
         "IMPRESSIONS" (sorting-with-column :impressions)]
-       [:th {:class "border border-[#343434]-600 text-justify"
+       [:th {:class "border border-[#343434] text-justify border-x-0"
              :on-click #(re-frame/dispatch
                          [::events/sort-advertisers :clicks])}
         "CLICKS" (sorting-with-column :clicks)]]]
@@ -50,32 +51,32 @@
       (cond
         error-state?
         [:tr
-         [:td {:class "border border-[#343434]-700"} "Could not load data :("]]
+         [:td {:class "border border-[#343434] border-x-0"} "Could not load data :("]]
         (or loading? (nil? loading?))
         [:tr
-         [:td {:class "border border-[#343434]-700"} "Loading..."]]
+         [:td {:class "border border-[#343434] border-x-0"} "Loading..."]]
         :else
         (cons
          [:<> {:key "random-key"}]
          (mapv (fn [{:keys [name createdAt campaignIds impressions clicks id]}]
                  [:tr
-                  {:key id}
+                  {:key id :class "hover:bg-[#313131]"}
                   [:td
-                   {:class "border border-[#343434]-700"} name]
+                   {:class "border border-[#343434] border-x-0"} name]
                   [:td
-                   {:class "border border-[#343434]-700"} createdAt]
+                   {:class "border border-[#343434] border-x-0"} createdAt]
                   [:td
-                   {:class "border border-[#343434]-700"} (count campaignIds)]
+                   {:class "border border-[#343434] border-x-0"} (count campaignIds)]
                   [:td
-                   {:class "border border-[#343434]-700"} (or impressions
-                                                              "n/a")]
+                   {:class "border border-[#343434] border-x-0"} (or impressions
+                                                                     "n/a")]
                   [:td
-                   {:class "border border-[#343434]-700"} (or clicks
-                                                              "n/a")]])
+                   {:class "border border-[#343434] border-x-0"} (or clicks
+                                                                     "n/a")]])
                advertisers)))]]))
 
 (defn overview []
   [:div {:class "flex min-w-full flex-col text-slate-300 justify-center items-center"}
-   [:div {:class "flex flex-col m-5 text-slate-300 w-11/12 border-slate-700 border-y-2 border-x-2 justify-center items-center bg-[#222]"}
-    [:p {:class "w-11/12 m-5 p-4 border-b-slate-700 border-b-2"} "Overview of dvertisers"]
+   [:div {:class "flex flex-col m-5 text-slate-300 w-11/12 border-[#343434] border-y-2 border-x-2 justify-center items-center bg-[#222]"}
+    [:p {:class "w-11/12 m-5 p-4 border-b-[#343434] border-b-2"} "Overview of dvertisers"]
     [table]]])
